76ead1a117038cf05b26e607f289bb3846c0f62d
Gabe - Desktop <glopez11193@gmail.com>
2020-07-12 22:20:49 -0500

    Add cryptojs

---
+++ WageringGG/Client/wwwroot/lib/dist/js/site.js
@@ -0,0 +1,65 @@
+﻿//FUNCTIONS
+window.copyToClipboard = async (text) => {
+	await navigator.clipboard.writeText(text);
+};
+
+//BLAZOR
+window.blazorCollapse = (id, action) => {
+	$(id).collapse(action);
+};
+window.blazorModal = (id, action) => {
+	$(id).modal(action);
+};
+
+window.blazorConfirm = (message) => {
+	return confirm(message);
+};
+
+window.encryptSecret = (secret, code) => {
+	var cipher = CryptoJS.AES.encrypt(secret, code).toString();
+	localStorage.setItem("EncryptedSecret", cipher);
+};
+
+window.decryptSecret = async (code) => {
+	var cipher = localStorage.getItem("EncryptedSecret");
+	var bytes = CryptoJS.AES.decrypt(cipher, code);
+	await copyToClipboard(bytes.toString(CryptoJS.enc.Utf8));
+};
+
+//STELLAR
+window.StellarSdk = StellarSdk
+
+window.signTransaction = (tx, secret, phrase) => {
+	const clientKeys = StellarSdk.Keypair.fromSecret(secret);
+	const transaction = StellarSdk.TransactionBuilder.fromXDR(tx, phrase);
+	transaction.sign(clientKeys);
+	return transaction.toXDR();
+};
+
+//NOTIFICATION
+window.getNotification = (title, text) => {
+	const icon = "https://jgz84g.dm.files.1drv.com/y4mmBczk2XUOMHGwQtzk2VP0m_COC6fHpbNC9eJ25LY5dgwy9BNQUuWo6jA9vHHVHqRPLF2WmkAaRpo06NxbTNb2Vqk9DcCEnrEFU6UuyVOjnD9RfDz1yoSdF7-t0DcF8JH9pUNQqLkkd8FrDnL4bc1K8nvX8tbAzC0kaU1YmleOmfyA6SR0JJODeIArZgs5sm9WzArPDdNrlMyAl0rPLuLKQ?width=512&height=512&cropmode=none";
+	if (window.Notification) {
+		// check if permission is already granted
+		if (Notification.permission === 'granted') {
+			// show notification here
+			var notify = new Notification(title, {
+				body: text,
+				icon: icon,
+			});
+		} else {
+			// request permission from user
+			Notification.requestPermission().then(function (p) {
+				if (p === 'granted') {
+					// show notification here
+					var notify = new Notification(title, {
+						body: text,
+						icon: icon,
+					});
+				}
+			}).catch(function (err) {
+				console.error(err);
+			});
+		}
+	}
+};
